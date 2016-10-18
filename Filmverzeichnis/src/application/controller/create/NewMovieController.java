49de8c415a65e725a87c121628412b83399000de
ColinWojciechowski <C.Wojciechowski@gmx.net>
2016-10-18 09:55:53 +0200

    NewMovie validation works user friendly

--- Filmverzeichnis/src/application/controller/create/NewMovieController.java
+++ Filmverzeichnis/src/application/controller/create/NewMovieController.java
@@ -42,15 +42,27 @@ public class NewMovieController {
 
    @FXML
    public void btnOkClicked() {
+      boolean yearValid = false;
+      boolean txtValid = false;
       try {
-         this.year.set(Integer.parseInt(txtYear.textProperty().get()));
-         viewModel.addMovie();
-         removeArguments();
-         txtYear.setPromptText("Year");
-         MainObservable.toggleMovie();
-      } catch (NumberFormatException | NullPointerException e) {
+         this.year.set(Integer.parseInt(txtYear.getText()));
+         yearValid = true;
+      } catch (NumberFormatException e) {
          txtYear.clear();
-         txtYear.setPromptText("Bitte nur Zahlen eingeben!");
+         txtYear.setPromptText("Bitte Jahreszahl eingeben!");
+      }
+      try {
+         if (txtTitle.getText().isEmpty() || txtGenre.getText().isEmpty())
+            throw new NullPointerException();
+         viewModel.addMovie();
+         txtValid = true;
+      } catch (NullPointerException e) {
+         txtTitle.setPromptText(txtTitle.getText().isEmpty() ? "Titel - Pflichtfeld" : "Title");
+         txtGenre.setPromptText(txtGenre.getText().isEmpty() ? "Genre - Pflichtfeld" : "Genre");
+      }
+      if (yearValid && txtValid) {
+         removeArguments();
+         MainObservable.toggleMovie();
       }
    }
 
