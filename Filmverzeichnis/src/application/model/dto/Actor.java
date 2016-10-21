865a13314162dc038002cd8f9674eed341295072
ColinWojciechowski <C.Wojciechowski@gmx.net>
2016-10-21 09:25:29 +0200

    Update Documentation
    Ausprobieren von Javadocs an der Actor-Klasse

--- Filmverzeichnis/src/application/model/dto/Actor.java
+++ Filmverzeichnis/src/application/model/dto/Actor.java
@@ -10,59 +10,132 @@ import javafx.beans.property.StringProperty;
  * Created by Kay Gerlitzki on 05.10.2016.
  */
 
-public class Actor implements Comparable<Actor>{
-    private int id;
-    private StringProperty name = new SimpleStringProperty();
-    private StringProperty birthDate = new SimpleStringProperty();
-//    private Date birthDate;
-    private StringProperty sex = new SimpleStringProperty();
-//    private Sex sex;
-    private List<Movie> movies;
+/**
+ * DTO fuer die Verarbeitung der Daten eines Schauspielers
+ *
+ * @author Kay Gerlitzki
+ * @version 1.0
+ */
+public class Actor implements Comparable<Actor> {
 
-    public int getId() {
-        return id;
-    }
+   /** Identifikationsnummer des Schauspielers */
+   private int id;
 
-    public void setId(int id) {
-        this.id = id;
-    }
+   /** Name des Schauspielers */
+   private StringProperty name = new SimpleStringProperty();
+   /** Geburtstag des Schauspielers */
+   private StringProperty birthDate = new SimpleStringProperty();
+   /** Geschlecht des Schauspielers */
+   private StringProperty sex = new SimpleStringProperty();
+   /** Liste aller Filme, in denen der Schauspieler mitwirkt */
+   private List<Movie> movies;
 
+   /**
+    * Laden der Identifikationsnummer
+    *
+    * @return int
+    * @since 1.0
+    */
+   public int getId() {
+      return id;
+   }
 
+   /**
+    * Setzen des Identifikationsnummer
+    *
+    * @param id Identifikationsnummer
+    * @since 1.0
+    */
+   public void setId(int id) {
+      this.id = id;
+   }
 
-    public StringProperty getName() {
+   /**
+    * Laden des Namens
+    *
+    * @return StringProperty
+    * @since 1.0
+    */
+   public StringProperty getName() {
       return name;
    }
 
+   /**
+    * Setzen des Namens
+    *
+    * @param name Namen
+    * @since 1.0
+    */
    public void setName(StringProperty name) {
       this.name = name;
    }
 
+   /**
+    * Laden des Geburtstags
+    *
+    * @return StringProperty
+    * @since 1.0
+    */
    public StringProperty getBirthDate() {
-        LocalDate d = LocalDate.now();
-        birthDate.set(d.toString());
-        return birthDate;
-    }
+      LocalDate d = LocalDate.now();
+      birthDate.set(d.toString());
+      return birthDate;
+   }
 
-    public void setBirthDate(StringProperty birthDate) {
-        this.birthDate = birthDate;
-    }
+   /**
+    * Setzen des Geburtstags
+    *
+    * @param birthDate Geburtstags
+    * @since 1.0
+    */
+   public void setBirthDate(StringProperty birthDate) {
+      this.birthDate = birthDate;
+   }
 
-    public StringProperty getSex() {
-        return sex;
-    }
+   /**
+    * Laden des Geschlechts
+    *
+    * @return StringProperty
+    * @since 1.0
+    */
+   public StringProperty getSex() {
+      return sex;
+   }
 
-    public void setSex(String sex) {
-        this.sex.set(sex);
-    }
+   /**
+    * Setzen des Geschlechts
+    *
+    * @param sex Geschlechts
+    * @since 1.0
+    */
+   public void setSex(String sex) {
+      this.sex.set(sex);
+   }
 
+   /**
+    * Laden der Liste von Filmen
+    *
+    * @return List
+    * @since 1.0
+    */
    public List<Movie> getMovies() {
       return movies;
    }
 
+   /**
+    * Setzen des Liste von Filmen
+    *
+    * @param movies Liste von Filmen
+    * @since 1.0
+    */
    public void setMovies(List<Movie> movies) {
       this.movies = movies;
    }
 
+   /*
+    * (non-Javadoc)
+    * @see java.lang.Comparable#compareTo(java.lang.Object)
+    */
    @Override
    public int compareTo(Actor actor) {
       return this.name.get().compareTo(actor.getName().get());
